e017e36b225e527203cd164cd29c027cfba4df6b
isai <isaiorellana.dev@gmail.com>
2022-05-31 13:22:25 -0600

    ✨ Envio de formulario con netlify

--- src/components/ContactameDev.js
+++ src/components/ContactameDev.js
@@ -2,17 +2,27 @@ import React from "react"
 
 const ContactameDev = ({ clase }) => {
   return (
-    <form className={clase}>
+    <form className={clase} name="contacto-isai-dev" netlify>
       <h2>Envíame un Mensaje</h2>
       <div className="from">
-        <label htmlFor>De:</label>
-        <input placeholder="micorreo@dominio.com"></input>
+        <label htmlFor="email">De:</label>
+        <input
+          placeholder="micorreo@dominio.com"
+          type="email"
+          name="email"
+          id="email"
+        ></input>
       </div>
       <div className="message">
-        <label htmlFor>Mensaje:</label>
-        <textarea placeholder="Hola Isaí, me contacto contigo porque..."></textarea>
+        <label htmlFor="message">Mensaje:</label>
+        <textarea
+          placeholder="Hola Isaí, me contacto contigo porque..."
+          type="message"
+          name="message"
+          id="message"
+        ></textarea>
       </div>
-      <button>Enviar</button>
+      <button type="submit">Enviar</button>
     </form>
   )
 }
