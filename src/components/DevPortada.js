ed1e7118cc4d6a0ac53131c9ba9298847dbfcc7b
isai <isaiorellana.dev@gmail.com>
2022-05-31 09:40:00 -0600

    💫 ANimacion a portada dev

--- src/components/DevPortada.js
+++ src/components/DevPortada.js
@@ -5,13 +5,17 @@ import "../styles/components/DevPortada.scss"
 const DevPortada = () => {
   return (
     <section className="dev-portada">
-      <h1>
-        ISAI <span>ORELLANA</span>
-      </h1>
-      <p>Desarrollador Web Frontend</p>
-      <Link to="" className="contactame-btn">
-        Contactame ✉️
-      </Link>
+      <div className="dev-portada--content">
+        <h1>
+          ISAI <span>ORELLANA</span>
+        </h1>
+        <p>Desarrollador Web Frontend</p>
+        <Link to="" className="contactame-btn">
+          Contactame ✉️
+        </Link>
+      </div>
+      <div className="detail-1"></div>
+      <div className="detail-2"></div>
     </section>
   )
 }
